fbf3fbe20f6be3d67c72830c9ebb006776cb5062
Hemant Bhardwaj <hemant.bhardwaj@ext.airbnb.com>
2023-01-18 20:47:53 +0530

    Added dependency to discovery server

--- movie-info-service/src/main/java/com/meta/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/meta/movieinfoservice/MovieInfoServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableAutoConfiguration
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
